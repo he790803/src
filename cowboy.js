a81e498c2744bde85ea9b689e6dc7c4b020e5e40
he798003 <39526456+he798003@users.noreply.github.com>
2018-05-22 23:13:33 +0800

    網站架構

---
+++ cowboy.js
@@ -0,0 +1,11 @@
+function handler() {
+    if (document.getElementsByClassName("nam01")[0].innerHTML=="Next Works 251") {
+        document.getElementsByClassName("myVideo")[0].setAttribute("src","https://www.youtube.com/embed/_Nk8vcOwXsA");
+    }
+}
+function handler1() {
+    if(document.getElementsByClassName("nam01")[1].innerHTML=="LIN DONG CHANNEL / 概念介紹篇"){
+        document.getElementsByClassName("myVideo")[0].setAttribute("src","https://www.youtube.com/embed/YYw0KCTFAIY"); 
+    }
+   
+}
